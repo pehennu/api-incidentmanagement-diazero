221f893c2bbad3158a88ed7ddb8a81ce882cb12e
nunes <pehennu@gmail.com>
2025-05-07 17:09:54 -0300

    feat: commits iniciais

---
+++ src/test/java/com/br/incidentmanagement/IncidentmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.incidentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
